a3615e43b94b13c6d96f1e3ed24191375f2a94f1
priyavarat saini <priyavartsaini3@gmail.com>
2024-09-07 10:02:12 +0530

    function

--- JavaScript/03_basics/01_functions.js
+++ JavaScript/03_basics/01_functions.js
@@ -1,11 +1,12 @@
 //function sayMyName(){
-    // console.log("P");
-    // console.log("R");
-    // console.log("I");
-    // console.log("N");
-    // console.log("C");
-    // console.log("E");
-// }
+//    console.log("P");
+//    console.log("R");
+//    console.log("I");
+//    console.log("N");
+//    console.log("C");
+//    console.log("E");
+//  }
+//sayMyName()
 
 //function addTwoNumbers(number1, number2){
 //    console.log(number1 + number2)
@@ -34,3 +35,37 @@ function loginUserMessage(username = "sam"){
 }
 
 console.log(loginUserMessage("prince"));
+
+function calculateCartPrice(val1, val2, ...num1){
+   // return `${val1}, ${val2}, ${num1}`
+   return num1
+    
+}
+
+//console.log(calculateCartPrice(300, 400,500, 600));
+
+const user = {
+    username: "prince",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 500, 600,]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 300, 500 ,699]));
+
+
