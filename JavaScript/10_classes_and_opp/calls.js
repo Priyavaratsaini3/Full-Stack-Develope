a63367415be0f57de2c4a947e8c4c8151eaf3dc3
priyavarat saini <priyavartsaini3@gmail.com>
2024-11-13 06:39:00 +0530

    how to call the functions

---
+++ JavaScript/10_classes_and_opp/calls.js
@@ -0,0 +1,14 @@
+function SetUsername(username){
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username, email, password){
+    SetUsername.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@google.com", 123)
+console.log(chai);
